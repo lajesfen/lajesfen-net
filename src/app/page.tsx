93aeac829f688b0dd5a95e97fb76b4c055ba053a
lajesfen <luciano.aguirre@utec.edu.pe>
2025-10-05 17:06:30 -0500

    fix: Remove absolute image position

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,19 +13,19 @@ const projects = Array.from({ length: 0 }, (_, i) => `Project ${i + 1}`);
 
 export default function Home() {
   return (
-    <main className="relative min-h-screen flex flex-col items-center justify-center overflow-hidden">
-      <section className="fixed pointer-events-none select-none mb-10 -z-10">
+    <main className="relative min-h-screen flex flex-col items-center justify-center overflow-hidden gap-4">
+      <section className="pointer-events-none select-none -z-10">
         <Image
           src={"/graphics/pc.png"}
           alt="pc"
           width={1920}
-          height={1080}
+          height={570}
           priority
           draggable={false}
         />
       </section>
-      <section className="absolute bottom-20 flex flex-col gap-12 items-center px-4 text-xs md:text-base">
-        <div className="flex flex-wrap justify-center items-center gap-4">
+      <section className="flex flex-col gap-12 items-center p-2 text-xs md:text-base">
+        <div className="grid grid-cols-3 md:grid-cols-6 justify-center justify-items-center items-center gap-4">
           {projects.length != 0 ? (
             projects.map((project, index) => (
               <a key={index} href={`#project-${index + 1}`}>
@@ -33,10 +33,10 @@ export default function Home() {
               </a>
             ))
           ) : (
-            <p>No projects (yet)</p>
+            <></>
           )}
         </div>
-        <footer className="flex justify-center items-center gap-6">
+        <footer className="flex flex-row justify-center items-center gap-4">
           {socialLinks.map((link) => (
             <a
               key={link.name}
